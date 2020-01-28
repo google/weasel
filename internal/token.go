65182340917fdbf48c77825be16fb4aae981f9bd
marcacohen <marcacohen@gmail.com>
2020-01-28 15:18:41 +0000

    fix review comments

--- internal/token.go
+++ internal/token.go
@@ -2,19 +2,18 @@
 package internal
 
 import (
-	"golang.org/x/oauth2/google"
-	"golang.org/x/net/context"
+	"context"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 // AETokenSource returns App Engine OAuth2 token source
 // given a context.Context and a slice of scopes.
 // It is a stubbed static token source during testing.
-func tmp(ctx context.Context, scope ...string) oauth2.TokenSource {
+var AETokenSource = func(ctx context.Context, scope ...string) oauth2.TokenSource {
   ts, err := google.DefaultTokenSource(ctx, scope...)
   if (err != nil) {
     panic(err)
   }
   return ts
 }
-var AETokenSource = tmp
