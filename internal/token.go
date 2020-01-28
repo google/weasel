be6ce73d8d58a4a340371eb7919ae074e43f7c25
marcacohen <marcacohen@gmail.com>
2020-01-28 14:12:59 +0000

    upgrade module to work w/ go 1.11

--- internal/token.go
+++ internal/token.go
@@ -1,9 +1,20 @@
 // Package internal contains utilities internal to the weasel packages.
 package internal
 
-import "golang.org/x/oauth2/google"
+import (
+	"golang.org/x/oauth2/google"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
 
 // AETokenSource returns App Engine OAuth2 token source
 // given a context.Context and a slice of scopes.
 // It is a stubbed static token source during testing.
-var AETokenSource = google.AppEngineTokenSource
+func tmp(ctx context.Context, scope ...string) oauth2.TokenSource {
+  ts, err := google.DefaultTokenSource(ctx, scope...)
+  if (err != nil) {
+    panic(err)
+  }
+  return ts
+}
+var AETokenSource = tmp
